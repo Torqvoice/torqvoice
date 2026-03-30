a297ed518999fcce8cf9084c8b934d52211c01af
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2026-03-30 06:24:48 +0000

    connection pool

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -8,6 +8,9 @@ const globalForPrisma = globalThis as unknown as {
 function createPrismaClient() {
   const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL,
+    max: 10,
+    connectionTimeoutMillis: 5000,
+    idleTimeoutMillis: 30000,
   });
   return new PrismaClient({
     adapter,
